1a2cf847b6c92c4b80e44f72037237d569250f1d
Edwin Dominic Joseph <edj3650@gmail.com>
2025-06-12 14:24:04 +0100

    Test husky lint-staged backend

---
+++ Backend/src/Books/test.js
@@ -0,0 +1,9 @@
+var items = [1, 2, 3, 4];
+items.map((item) => {
+  console.log("Item: " + item);
+});
+function sayHello(name) {
+  return "Hello " + name;
+}
+const user = "ChatGPT";
+console.log(sayHello(user));
