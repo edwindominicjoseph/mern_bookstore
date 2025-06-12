03561f3335b726c4eb03d0b124470a40b778dc2a
Edwin Dominic Joseph <edj3650@gmail.com>
2025-06-12 19:30:27 +0100

    Test GitHub Actions

--- Backend/src/Books/book.route.js
+++ Backend/src/Books/book.route.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const Book = require('./book.model');
-const { postABook, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
-const { get } = require('mongoose');
+const express = require("express");
+const {
+  postABook,
+  getSingleBook,
+  UpdateBook,
+  deleteABook,
+} = require("./book.controller");
 const router = express.Router();
-const { getAllBooks } = require('./book.controller');
+const { getAllBooks } = require("./book.controller");
 
 // Logic==> Frontend sends a post request--- backend server--- book route---book.controller.js---book.model.js---(if accepts)--save to db and send a success message--if any errors on the path it will be caught and a failure message will be sent back to the frontend.
 
-router.post('/create-book',postABook);
+router.post("/create-book", postABook);
 
 // get all books
-router.get('/', getAllBooks);
+router.get("/", getAllBooks);
 
 // get a single book
-router.get('/:id' , getSingleBook)
+router.get("/:id", getSingleBook);
 
 // update a book
-router.put('/edit/:id', UpdateBook )
+router.put("/edit/:id", UpdateBook);
 
 // delete a book
-router.delete('/:id', deleteABook)
-    
+router.delete("/:id", deleteABook);
 
 module.exports = router;
