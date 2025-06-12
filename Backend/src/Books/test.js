efc3e1fd1418bdbfe42bea1fa63c207e23fc630d
Edwin Dominic Joseph <edj3650@gmail.com>
2025-06-12 14:50:40 +0100

    code quality workflow added

--- Backend/src/Books/test.js
+++
@@ -1,9 +0,0 @@
-var items = [1, 2, 3, 4];
-items.map((item) => {
-  console.log("Item: " + item);
-});
-function sayHello(name) {
-  return "Hello " + name;
-}
-const user = "ChatGPT";
-console.log(sayHello(user));
--- Frontend/src/components/test.jsx
+++
@@ -1,5 +0,0 @@
-let result = 0;
-for (let i = 0; i < 5; i++) {
-  result += i;
-}
-console.log("Sum is " + result);
