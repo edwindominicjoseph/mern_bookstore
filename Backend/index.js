f26ac735f89147fecf623ec533b49cdf1a37899a
Edwin Dominic Joseph <edj3650@gmail.com>
2025-06-16 22:21:31 +0100

    dockerised frontend and backend

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:3000"],
     credentials: true,
   }),
 );
--- Frontend/src/redux/features/orders/orderapi.js
+++ Frontend/src/redux/features/orders/orderapi.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import getBaseUrl from "../../../utils/baseURL";
+import getBaseUrl from "../../../utils/baseUrl";
 
 const orderApi = createApi({
   reducerPath: "orderApi",
