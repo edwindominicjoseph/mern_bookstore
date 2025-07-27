6e82f7692b1afb70915b38da0c419c3e1e99b33d
Edwin Dominic Joseph <edj3650@gmail.com>
2025-07-27 11:03:05 +0100

    fix: set production API base URL

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,11 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+    ],
     credentials: true,
   }),
 );
